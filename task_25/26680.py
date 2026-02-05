fef4a6893ef51d5255006149b2db5c201478f20d
gymi <Glebastien@yandex.ru>
2026-02-05 17:37:46 +0200

    klaswork by Gymigleb

--- task_25/26680.py
+++ task_25/26680.py
@@ -1,13 +1,26 @@
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5) + 1):
+        if n%i == 0: return False
+    return True
+
 def fact(n):
     out = []
-    while n % 2 == 0:
-        out.append(2)
-        n //= 2
-    for d in range(3, int(n**0.5) + 1, 2):
-        while n % d == 0:
-            out.append(d)
-            n //= d
+    i = 3
+    while i < int(n**0.5) + 1:
+        while n % i == 0:
+            out.append(i)
+            n //= i
+        i+=2
     if n > 2: out.append(n)
     return out
 
-print(fact(1))
+cnt = 0
+
+for n in range(5000001, 10**20, 2):
+    d = fact(n)
+    if len(d) == 2 and len(d) == len(set(d)):
+        if is_prime(abs(d[0] - d[1])):
+            print(n, max(d))
+
+    if cnt >= 5: break
