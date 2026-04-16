c976aa26e4dc25d91c2986d027111acccedb2b4b
gymi <Glebastien@yandex.ru>
2026-04-16 17:30:29 +0200

    klasswork by Gymigleb

---
+++ task_27/23209.py
@@ -0,0 +1,52 @@
+from math import dist
+
+def sum_l(stA, KL):
+    return sum(dist(stA, stB) for stB in KL)
+
+def center(KL):
+    out = []
+    for stA in KL:
+        sum_dist = sum_l(stA, KL)
+        out.append(([sum_dist], stA))
+    return min(out)[1]
+
+def A():
+    with open("./files/27_A_23209.txt") as f:
+        data = [list(map(float, i.replace(",", ".").split())) for i in f]
+
+    KLS = [[], []]
+
+    for st in data:
+        KLS[0 if st[0] < 5 else 1].append(st)
+
+    centers = []
+    for KL in KLS: centers.append(center(KL))
+
+    px = int(max(st[0] for st in centers) * 10000)
+    py = int(max(st[1] for st in centers) * 10000)
+
+    print(px, py)
+
+def B():
+    with open("./files/27_B_23209.txt") as f:
+        data = [list(map(float, i.replace(",", ".").split())) for i in f]
+
+    KLS = [[], [], [], []]
+
+    for st in data:
+        x, y = st
+        KLS[3 if x < 5 or x > 17 else
+            0 if y < 13 else
+            1 if y < 21 else
+            2].append(st)
+
+    centers = []
+    for KL in KLS: centers.append(center(KL))
+
+    # print([len(KL) for KL in KLS])
+    qx = int(abs(centers[2][0] - centers[0][0]) * 10000)
+    qy = int(abs(centers[2][1] - centers[0][1]) * 10000)
+    print(qx, qy)
+
+A()
+B()
